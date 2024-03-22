8ab21d792b6a4e215ce25ed9115d219fea4fa2c4
macs1756 <kindrativmaksum312@gmail.com>
2024-03-22 20:05:58 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './graphql/models/User';
+import { UserSetting } from './graphql/models/UserSetting';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+          driver: ApolloDriver,
+          autoSchemaFile: 'src/schema.gql',
+        }),
+        ConfigModule,
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_LOGIN'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User, UserSetting],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/graphql/resolvers/UserSettingsResolver.ts
@@ -0,0 +1,20 @@
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { UserSetting } from '../models/UserSetting';
+import { CreateUserSettingsInput } from '../utils/CreateUserSettingsInput';
+import { UserSettingService } from '../../users/UserSettingService';
+
+@Resolver()
+export class UserSettingsResolver {
+  constructor(private userSettingsService: UserSettingService) {}
+
+  @Mutation(() => UserSetting)
+  async createUserSettings(
+    @Args('createUserSettingsData')
+    createUserSettingsData: CreateUserSettingsInput,
+  ) {
+    const userSetting = await this.userSettingsService.createUserSettings(
+      createUserSettingsData,
+    );
+    return userSetting;
+  }
+}
