11532c5c116dae053fe972e22b0274b713d60b58
macs1756 <kindrativmaksum312@gmail.com>
2024-04-07 18:55:29 +0300

    v18:55

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -2,22 +2,34 @@ import { WebSocketGateway, SubscribeMessage, WebSocketServer, OnGatewayInit } fr
 import { OnModuleInit } from '@nestjs/common';
 import { Server } from 'socket.io';
 
-@WebSocketGateway(80)
+@WebSocketGateway(80, {
+  cors: {
+    origin: 'http://localhost:3000/'
+  }
+})
 export default class MyGateway implements OnGatewayInit {
   @WebSocketServer()
   server: Server;
 
-
+  //inherited from core
   afterInit() {
+
+    //listen connect fronend
     this.server.on('connection', socket => {
       this.createNewValue()
     });
   }
 
+  //event to subscribe
   @SubscribeMessage('graph')
   createNewValue() {
     const sendGraphData = () => {
-      this.server.emit('graph', 'test');
+      
+      const randomValue = Math.floor(Math.random() * 100) + 1
+
+      //give back
+      this.server.emit('graph', randomValue);
+
       setTimeout(sendGraphData, 3000);
     };
 
